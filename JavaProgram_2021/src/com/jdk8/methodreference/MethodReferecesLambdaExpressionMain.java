c6094a91c22eaeaf7551efb3fd0d2bbfef887e8e
Satish Prasad <spd61185@gmail.com>
2021-01-18 15:09:26 +0530

    Method Reference

--- JavaProgram_2021/src/com/jdk8/methodreference/MethodReferecesLambdaExpressionMain.java
+++ JavaProgram_2021/src/com/jdk8/methodreference/MethodReferecesLambdaExpressionMain.java
@@ -2,10 +2,23 @@ package com.jdk8.methodreference;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.function.Consumer;
 
 public class MethodReferecesLambdaExpressionMain {
 	public static void main(String[] args) {
 		List<String> countryList =  Arrays.asList(new String[] {"India", "China","Nepal","Russia"});
-		
+		System.out.println("=======================");
+        System.out.println("Using anonymous class");
+        System.out.println("=======================");
+        
+        //Using anonymous class
+        countryList.stream().forEach(
+        		new Consumer<String>() {
+
+					@Override
+					public void accept(String country) {
+						System.out.println(country);
+					}
+				});
 	}
 }
