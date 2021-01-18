81f6f51c1ae53a3ef49b257cf5bf2798b00f70cd
Satish Prasad <spd61185@gmail.com>
2021-01-18 15:06:28 +0530

    Method Reference

---
+++ JavaProgram_2021/src/com/jdk8/methodreference/MethodReferecesLambdaExpressionMain.java
@@ -0,0 +1,11 @@
+package com.jdk8.methodreference;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class MethodReferecesLambdaExpressionMain {
+	public static void main(String[] args) {
+		List<String> countryList =  Arrays.asList(new String[] {"India", "China","Nepal","Russia"});
+		
+	}
+}
